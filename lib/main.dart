a664bf3925a7b509f8f2c6b4fcd828efc02e9d32
wellwhiskye-maker <wellwhiskye@gmail.com>
2026-05-27 23:36:27 +0800

    update project

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'NUGRAHA_2323050026/row.dart' as nugraha;
 import 'Mika_2323050007/row.dart' as mika;
 import 'Buda_2323050001/dashboard.dart';
+import 'MADEILALALITA_2323050013/home_page.dart' as ila;
 
 void main() {
   runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: DashboardPage()),
+    const MaterialApp(debugShowCheckedModeBanner: false, home: ila.HomePage()),
   );
 }
