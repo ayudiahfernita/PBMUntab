d771c7c77a215f847a31edd0f89e114837318a3d
wellwhiskye-maker <wellwhiskye@gmail.com>
2026-05-30 11:44:36 +0800

    update navigation mika

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'angelo_2323050005/statefulpage.dart';
 
 import 'NUGRAHA_2323050026/row.dart' as nugraha;
-import 'Mika_2323050007/row.dart' as mika;
 import 'Buda_2323050001/dashboard.dart';
 import 'MADEILALALITA_2323050013/home_page.dart' as ila;
 import 'NUGRAHA_2323050026/Tugas_2/main_navigation.dart';
